449eafc37514e852392286839e6afe174e795983
Francisco Dias <fdias@opera.com>
2022-09-29 16:36:59 +0100

    Fixed build_linux64.sh script

--- source/build_linux64.sh
+++ source/build_linux64.sh
@@ -6,9 +6,9 @@ g++ -m64 \
     Steamworks_vs/Steamworks/GMLSteam/steam_*.cpp \
     -Wl,-rpath,assets/ -fPIC \
 	-Ijson-c-0.9/ \
-    -Lsteamworks_sdk/redistributable_bin/linux64 \
+    -LSteamworks_sdk/redistributable_bin/linux64 \
     -lsteam_api \
-    -Isteamworks_sdk/public/steam \
+    -ISteamworks_sdk/public/steam \
 	-ISteamworks_vs/Steamworks/GMLSteam/ \
 	-ISteamworks_vs/Steamworks/ \
     -shared -o Steamworks_gml/extensions/Steamworks/Steamworks.so
