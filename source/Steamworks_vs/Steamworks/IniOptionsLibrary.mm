798c1fe8bda7ec328cf04e00bdc4b49b836dbe86
Francisco Dias <fdias@opera.com>
2022-11-29 16:22:38 +0000

    Changes to allow proper Windows to macOS

--- source/Steamworks_vs/Steamworks/IniOptionsLibrary.mm
+++ source/Steamworks_vs/Steamworks/IniOptionsLibrary.mm
@@ -98,8 +98,29 @@ std::string IniOptions_read(std::string extensionName,std::string key)
         
         testPath = [[[NSProcessInfo processInfo] arguments][2] UTF8String];
         testPath = testPath.parent_path() / "options.ini";
-        
-        optionsIniPath = testPath;
+        if (std::filesystem::exists(testPath))
+        {
+            optionsIniPath = testPath;
+        }
+        else
+        {
+            // We are on Windows -> macOS build
+            
+            // 0: Path to MacOS runner
+            // 1: -game
+            // 2: pathToZip
+            // 3: -debugoutput
+            // 4: pathToDebug
+            // 5: -output
+            // 6: pathToOutput
+            // 7: -runTest
+            // 8: -game
+            // 9: pathToiOS.ios
+            
+            testPath = [[[NSProcessInfo processInfo] arguments][9] UTF8String];
+            testPath = testPath.parent_path() / "options.ini";
+            optionsIniPath = testPath;
+        }
     }
 
     std::string str = readFile(optionsIniPath);
