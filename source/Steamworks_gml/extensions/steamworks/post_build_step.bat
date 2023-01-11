f42e25c81a9d75f2b9780d85c612dc5cfce615c6
Francisco Dias <Dias.FranciscoA@gmail.com>
2023-01-11 12:02:36 +0000

    Updated the batch script for better debugging information
    Updated windows BATCH file.

--- source/Steamworks_gml/extensions/steamworks/post_build_step.bat
+++ source/Steamworks_gml/extensions/steamworks/post_build_step.bat
@@ -1,153 +1,251 @@
 @echo off
 
-:: Useful for printing all variables
-:: set
+goto :start
 
-:: ############################################## WARNING ##############################################
-::       THIS FILE IS SHOULD NOT BE CHANGED AND THE OPTIONS SHOULD BE CONTROLLED THROUGH THE IDE.
-:: #####################################################################################################
+:: ######################################################################################
+:: Auxiliar Functions
 
+:: Checks minimum product version for the 3 available builds (handles errors)
+:checkMinVersion current stable beta develop identifier
+call :compareVersions "%~1" "%~3" result
+if %result% geq 0 exit /b 0
+call :compareVersions "%~1" "%~2" result
+if %result% geq 0 exit /b 0
+call :compareVersions "%~1" "%~4" result
+if %result% geq 0 exit /b 0
+call :errorMinVersion "%~1" "STABLE v%~2 or BETA v%~3" "%~5"
+exit /b 0
 
-:: Read extension options or use default (development) value
-if "%YYEXTOPT_Steamworks_SteamSDK%" == "" (
-   set STEAM_SDK_PATH=%~dp0\..\..\..\steamworks_sdk\
-) else (
-   set STEAM_SDK_PATH=%YYEXTOPT_Steamworks_SteamSDK%
-)
+:: Resolves a relative path if required
+:pathResolve basePath relativePath result
+for /f "tokens=* usebackq" %%F in (`powershell -NoLogo -NoProfile -Command Push-Location '%~1'^; $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath^('%~2'^)^; Pop-Location`) do ( set "%~3=%%F" )
+exit /b 0
 
-:: Ensure the path ends with a backslash
-if not %STEAM_SDK_PATH:~-1% == \ (
-   set SDK_PATH=%STEAM_SDK_PATH%\
-) else (
-   set SDK_PATH=%STEAM_SDK_PATH%
-)
+:: Resolves an existing relative path if required (handles errors)
+:pathResolveExisting basePath relativePath result
+call :pathResolve "%~1" "%~2" test
+set "%~3=%test%"
+if not exist "%test%" call :errorPathInexistant "%test%"
+set "test="
+exit /b 0
 
-:: Check if debug mode is also 'Enabled' else is 'Auto' (use YYTargetFile hacks)
+:: Copies file to provided location (handles errors)
+:fileCopyTo srcFile destFolder
+powershell -NoLogo -NoProfile -Command Copy-Item -Path '%~1' -Destination '%~2' -Force && echo "Finished copying '%~nx1' to '%~2'"
+if ERRORLEVEL 1 call :errorFileCopy "%~1" "%~2"
+exit /b 0
+
+:: Extracts a zip file to a folder (handles errors)
+:fileExtract srcFile destFolder identifier
+powershell Expand-Archive '%~1' '%~2'
+if ERRORLEVEL 1 call :errorFileExtract "%~1" "%~2" "%~3"
+exit /b 0
+
+:: Compresses folder into a zip file (handles errors)
+:folderCompress srcFolder destFile identifier
+powershell Compress-Archive -Force '%~1' '%~2'
+if ERRORLEVEL 1 call :errorFolderCompress "%~1" "%~2" "%~2"
+exit /b 0
+
+:: Generates a SHA256 hash of a file and stores it into a variable
+:fileGetHash pathToFile result
+for /f "tokens=* usebackq" %%F in (`powershell -NoLogo -NoProfile -Command ^(Get-FileHash '%~1'^)[0].Hash`) do ( set "%~2=%%F" )
+exit /b 0
+
+:: Asserts a file hash
+:assertFileHash pathToFile hash name
+setlocal
+set name=%~nx1
+if "%~3" neq "" ( set name=%~3 )
+call :fileGetHash "%~1" result
+if "%result%" neq "%~2" call :errorHashMismatch "%name%"
+endlocal
+exit /b 0
+
+:: Compares two version numbers and saves result into variable
+:compareVersions version1 version2 result
+for /f "tokens=* usebackq" %%F in (`powershell -NoLogo -NoProfile -Command ^([System.Version]'%~1'^).compareTo^([System.Version]'%~2'^)`) do ( set "%~3=%%F" )
+exit /b 0
+
+:: Asserts an exact version number
+:assertExactVersion version reqVersion name
+setlocal
+set name=file
+if "%~3" neq "" ( set name=%~3 )
+call :compareVersions "%~1" "%~2" result
+if %result% neq 0 call :errorExactVersion "%1" "%2" "%3"
+endlocal
+exit /b 0
+
+:: Asserts a minimum version number
+:assertMinVersion version minVersion name
+setlocal
+set name=file
+if "%~3" neq "" ( set name=%~3 )
+call :compareVersions "%~1" "%~2" result 
+if %result% equ -1 call :errorMinVersion "%1" "%2" "%3"
+endlocal
+exit /b 0
+
+:: ######################################################################################
+:: Error Messages
+
+:errorExactVersion version reqVersion name
+echo "########################################## ERROR ##########################################"
+echo "# Invalid '%~3' version, requires '%~2' (got '%~1')"
+echo "###########################################################################################"
+exit 1
+
+:errorMinVersion version minVersion name
+echo "########################################## ERROR ##########################################"
+echo "# Invalid '%~3' version, requires at least '%~2' (got '%~1')"
+echo "###########################################################################################"
+exit 1
+
+:errorHashMismatch identifier
+echo "########################################## ERROR ##########################################"
+echo "# Invalid '%~1' version, sha256 hash mismatch. Please check documentation."
+echo "###########################################################################################"
+exit 1
+
+:errorPathInexistant fullpath
+echo "########################################## ERROR ##########################################"
+echo "# Invalid path '%~1%' does not exist."
+echo "###########################################################################################"
+exit 1
+
+:errorFileExtract filepath identifier
+setlocal
+call :pathResolve "%~1" fullpath
+set identifier=file
+if "%~2" neq "" ( set identifier=%~2 )
+echo "########################################## ERROR ##########################################"
+echo "# Failed to expand %identifier% '%fullpath%' (please file a bug on this issue)."
+echo "###########################################################################################"
+endlocal
+exit 1
+
+:errorFolderCompress folderpath identifier
+call :pathResolve "%~1" fullpath
+set identifier=folder
+if "%~2" neq "" ( set identifier=%~2 )
+echo "########################################## ERROR ##########################################"
+echo "# Failed to compress %identifier% '%fullpath%' (please file a bug on this issue)."
+echo "###########################################################################################"
+exit 1
+
+:errorDirectoryDelete fullpath
+call :pathResolve "%~1" fullpath
+echo "########################################## ERROR ##########################################"
+echo "# Failed to delete folder '%fullpath%' (please file a bug on this issue)."
+echo "###########################################################################################"
+exit 1
+
+:errorFileCopy source destination
+echo "########################################## ERROR ##########################################"
+echo "# Failed to copy file '%~1' to '%~2' (please file a bug on this issue)."
+echo "###########################################################################################"
+exit 1
+
+:: ######################################################################################
+:: Script Logic
+
+:start
+
+:: Version locks
+set MIN_VERSION_STABLE="2023.1.0.0"
+set MIN_VERSION_BETA="2023.100.0.0"
+set MIN_VERSION_DEV="9.9.1.293"
+
+:: SDK version v1.55
+set HASH_SDK_WIN="1DB3FD414039D3E5815A5721925DD2E0A3A9F2549603C6CAB7C49B84966A1AF3"
+set HASH_SDK_OSX="88DC79403F68E81B6674C927ED362EF3CF69046F587ED009FDC6AD85D85E97F2"
+set HASH_SDK_LINUX="C0CC3D2802E5F2463BFA0046C41D2F65A6335BAAEEFBBA6C7DBD5681D5CA7C46"
+
+:: Checks IDE and Runtime versions
+call :checkMinVersion "%YYruntimeVersion%" %MIN_VERSION_STABLE% %MIN_VERSION_BETA% %MIN_VERSION_DEV% runtime
+
+:: Resolve the SDK path (must exist)
+set SDK_PATH=%YYEXTOPT_Steamworks_SteamSDK%
+call :pathResolveExisting "%YYprojectDir%" "%SDK_PATH%" SDK_PATH
+
+:: Check if debug mode is 'Enabled' else is 'Auto' (use YYTargetFile hacks)
 set "DEBUG_MODE="
 if "%YYEXTOPT_Steamworks_Debug%" == "Enabled" set DEBUG_MODE=1
 if "%YYtargetFile%" == " " set DEBUG_MODE=1
 if "%YYtargetFile%" == "" set DEBUG_MODE=1
 
-:: Ensure the directory exists
-if not exist "%SDK_PATH%" goto error_incorrect_STEAMWORKS_path
-
 :: Ensure we are on the output path
 pushd "%YYoutputFolder%"
 
 :: Call setup method depending on the platform
-:: NOTE: the setup method can be (:Windows_copy_dependencies, :MacOS_copy_dependencies or :Ubuntu_copy_dependencies)
-call :%YYPLATFORM_name%_copy_dependencies
-if ERRORLEVEL 1 (
-    echo ""
-    echo "#################################### INFORMATION #####################################"
-    echo "Steam Extension is not available in this target: %YYPLATFORM_name% (no setup required)"
-    echo "######################################################################################"
-    echo ""
-)
+:: NOTE: the setup method can be (:setupWindows, :setupMacOS or :setupLinux)
+call :setup%YYPLATFORM_name%
+
 popd
 
-:exit
+exit 0
+
+:: ----------------------------------------------------------------------------------------------------
+:setupWindows
+
+    set SDK_SOURCE="%SDK_PATH%\redistributable_bin\win64\steam_api64.dll"
+    call :assertFileHash %SDK_SOURCE% %HASH_SDK_WIN% "Steam SDK"
+
+    echo "Copying Windows (64 bit) dependencies"
+    if not exist "steam_api64.dll" call :fileCopyTo %SDK_SOURCE% "steam_api64.dll"
+   
+    if defined DEBUG_MODE (
+        echo "Running a Windows Steamworks game project inside the Windows IDE, enabling Debug..."
+    )
+
 exit /b 0
 
 :: ----------------------------------------------------------------------------------------------------
-:Windows_copy_dependencies
-   if "%YYPLATFORM_option_windows_use_x64%" == "" (
+:setupMacOS
 
-      echo "Copying Windows (64 bit) dependencies"
-      if not exist "steam_api64.dll" copy "%SDK_PATH%redistributable_bin\win64\steam_api64.dll" "steam_api64.dll"
+    set SDK_SOURCE="%SDK_PATH%\redistributable_bin\osx\libsteam_api.dylib"
+    call :assertFileHash %SDK_SOURCE% %HASH_SDK_OSX% "Steam SDK"
 
-   ) else (
+    echo "Copying macOS (64 bit) dependencies"
+    if "%YYTARGET_runtime%" == "VM" (
+        :: This is used from VM compilation
+        call :fileExtract "%YYprojectName%.zip" "_temp\"
+        call :fileCopyTo %SDK_SOURCE% "_temp\assets\libsteam_api.dylib"
+        call :folderCompress "_temp\*" "%YYprojectName%.zip"
 
-      if "%YYPLATFORM_option_windows_use_x64%" == "True" (
-         echo "Copying Windows (64 bit) dependencies"
-         if not exist "steam_api64.dll" copy "%SDK_PATH%redistributable_bin\win64\steam_api64.dll" "steam_api64.dll"
-      ) else (
-         echo "Copying Windows (32 bit) dependencies"
-         if not exist "steam_api.dll" copy "%SDK_PATH%redistributable_bin\steam_api.dll" "steam_api.dll"
-      )
+        rmdir /s /q _temp
 
-   )
-   
-   if defined DEBUG_MODE (
-      echo "Running a Windows Steamworks game project inside the Windows IDE, enabling Debug..."
-	   :: do not put a space between > please, this breaks things!
-	   echo [SteamworksUtils]>>options.ini
-	   echo RunningFromIDE=True>>options.ini
-   )
-   
-   if ERRORLEVEL 1 call :exitError
-goto :eof
+    ) else (
+
+        :: This is used from YYC compilation
+        call :fileCopyTo %SDK_SOURCE% "%YYprojectName%\%YYprojectName%\Supporting Files\libsteam_api.dylib"
+    )
+
+    if defined DEBUG_MODE (
+        echo "Running a macOS %YYTARGET_runtime% Steamworks game project inside the Windows IDE, enabling Debug..."
+    )
+
+exit /b 0
 
 :: ----------------------------------------------------------------------------------------------------
-:macOS_copy_dependencies
-   echo "Copying macOS (64 bit) dependencies"
+:setupLinux
 
-   if "%YYTARGET_runtime%" == "VM" (
+    set SDK_SOURCE="%SDK_PATH%\redistributable_bin\osx\libsteam_api.dylib"
+    call :assertFileHash %SDK_SOURCE% %HASH_SDK_LINUX% "Steam SDK"
 
-      :: This is used for VM
-      powershell Expand-Archive '%YYprojectName%.zip' _temp\
-      copy /y "%SDK_PATH%redistributable_bin\osx\libsteam_api.dylib" "_temp\assets\libsteam_api.dylib"
-	  
-	   if defined DEBUG_MODE (
-		   echo "Running a macOS VM Steamworks game project inside the Windows IDE, enabling Debug..."
-		   :: do not put a space between > please, this breaks things!
-		   echo [SteamworksUtils]>>"_temp\assets\options.ini"
-		   echo RunningFromIDE=True>>"_temp\assets\options.ini"
-	   )
-	  
-      powershell Compress-Archive -Force _temp\* '%YYprojectName%.zip'
-      rmdir /s /q _temp
+    echo "Copying Linux (64 bit) dependencies"
+    
+    call :fileExtract "%YYprojectName%.zip" "_temp\"
+    if not exist "assets\libsteam_api.so" (
+        call :fileCopyTo %SDK_SOURCE% "_temp\assets\libsteam_api.so"
+        call :folderCompress "_temp\*" "%YYprojectName%.zip"
+    )
 
-   ) else (
+    if defined DEBUG_MODE (
+        echo "Running a Linux Steamworks game project inside the Windows IDE, enabling Debug..."
+    )
 
-      :: This is used from YYC compilation
-      copy "%SDK_PATH%redistributable_bin\osx\libsteam_api.dylib" "%YYprojectName%\%YYprojectName%\Supporting Files\libsteam_api.dylib"
-	  
-	   if defined DEBUG_MODE (
-		   echo "Running a macOS YYC Steamworks game project inside the Windows IDE, enabling Debug..."
-		   :: do not put a space between > please, this breaks things!
-		   echo [SteamworksUtils]>>"%YYprojectName%\%YYprojectName%\Supporting Files\options.ini"
-		   echo RunningFromIDE=True>>"%YYprojectName%\%YYprojectName%\Supporting Files\options.ini"
-	   )
-   )
-   if ERRORLEVEL 1 call :exitError
-goto :eof
+    rmdir /s /q _temp
 
-:: ----------------------------------------------------------------------------------------------------
-:Linux_copy_dependencies
-   echo "Copying Linux (64 bit) dependencies"
-   powershell Expand-Archive '%YYprojectName%.zip' _temp\
-
-   if defined DEBUG_MODE (
-      echo "Running a Linux Steamworks game project inside the Windows IDE, enabling Debug..."
-	   :: do not put a space between > please, this breaks things!
-      echo [SteamworksUtils]>>"_temp\assets\options.ini"
-      echo RunningFromIDE=True>>"_temp\assets\options.ini"
-   )
-
-   if not exist "assets/libsteam_api.so" (
-      copy "%SDK_PATH%redistributable_bin\linux64\libsteam_api.so" "_temp\assets\libsteam_api.so"
-      powershell Compress-Archive -Force _temp\* '%YYprojectName%.zip'
-   )
-   rmdir /s /q _temp
-   if ERRORLEVEL 1 call :exitError
-goto :eof
-
-:: ----------------------------------------------------------------------------------------------------
-:exitError
-   echo ""
-   echo "######################################################## ERROR #########################################################"
-   echo "The setup script was unable to copy dependencies"
-   echo "########################################################################################################################"
-   echo ""
-exit 1
-
-:: ----------------------------------------------------------------------------------------------------
-:: If the steamworks SDK path doesn't exit ask the user to edit this file
-:error_incorrect_STEAMWORKS_path
-   echo ""
-   echo "######################################################## ERROR #########################################################"
-   echo "The specified steamworks SDK path doesn't exist please edit the file 'post_build_step.bat' in your project's root folder"
-   echo "########################################################################################################################"
-   echo ""
-exit 1
+exit /b 0
