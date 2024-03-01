346f1cba076872fcdbab81df29b6b2294fa95469
Francisco Dias <fdias@opera.com>
2024-03-01 12:49:25 +0000

    Add missing indent

--- docs/documentation.js
+++ docs/documentation.js
@@ -9,9 +9,9 @@
  * 
  * @section Management
  * @desc This extension provides the following management functions:
- * * ${function.steam_init} (automatically called by the extension)
- * * ${function.steam_update} :warning: REQUIRED
- * * ${function.steam_shutdown} :warning: REQUIRED
+ *  * ${function.steam_init} (automatically called by the extension)
+ *  * ${function.steam_update} :warning: REQUIRED
+ *  * ${function.steam_shutdown} :warning: REQUIRED
  * @section_end
  * 
  * @section Modules
